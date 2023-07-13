32a7e3b3525b5208c3176307e15e825a4bfafeb7
Novrianto201510002 <limjaslyn25@gmail.com>
2023-07-13 19:56:40 +0700

    test

---
+++ application/views/front/partials/footer.php
@@ -0,0 +1,6 @@
+<footer class="section p-3 bg-dark text-white">
+    <div class="text-center">&copy; Roin Copyright <?php echo date("Y"); ?> Aplikasi Pemesanan Makanan</div>
+</footer>
+</body>
+
+</html>
